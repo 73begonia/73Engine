8287f274550d7942844f1ca456be18043e35c4ad
73begonia <1782015847@qq.com>
2022-07-05 19:32:29 +0800

    Setup basic Application and Entry Point.

---
+++ 73Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Engine applications
+
+#include "73Engine/Application.h"
+
+// ---Entry Point-----------------------
+#include "73Engine/EntryPoint.h"
+// -------------------------------------
